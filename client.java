d120e9d274f0971ba68fbbf4086b879488a6aed1
andreology27 <37252145+andreology@users.noreply.github.com>
2020-03-02 17:40:45 -0800

    first com

---
+++ client.java
@@ -0,0 +1,60 @@
+
+import java.net.*;
+import java.util.*;
+import java.io.*;
+//UDP client
+
+public class client {
+	public static void main(String args[]) throws Exception{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter an ip address");
+		String ipInput = in.next();
+		System.out.println("Please enter an port address");
+		int portInput = in.nextInt();
+
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please Enter a Message to Send to Server?");
+		DatagramSocket clientSocket = new DatagramSocket();
+		//InetAddress ip = InetAddress.getByName("localhost");
+		InetAddress ip = InetAddress.getByName(ipInput);
+		byte inData[] = new byte[1024];
+		byte outData[] = new byte[1024];
+		String data = input.readLine();
+		String message = new String("");
+
+
+
+
+		//port 1234
+
+		outData = data.getBytes();
+		DatagramPacket packet = new DatagramPacket(outData, outData.length, ip, portInput);
+		clientSocket.send(packet);
+		DatagramPacket packet2 = new DatagramPacket(inData, inData.length, ip, portInput);
+		clientSocket.receive(packet2);
+		String receiveData =new String(packet2.getData());
+		System.out.println(receiveData);
+		clientSocket.close();
+	}
+		/*
+		 * 		DatagramSocket aSocket = null;
+
+		try {
+			aSocket = new DatagramSocket();
+			byte[] m = args[0].getBytes();
+			InetAddress aHost = InetAddress.getByName(args[1]);
+			int serverPort = 6789;
+			DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
+			aSocket.send(request);
+			byte[] buffer = new byte[1000];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(reply);
+			System.out.println("Reply: " + new String(reply.getData()));
+
+		}catch (SocketException e) { System.out.println("Socket: " + e.getMessage());
+	} catch(IOException e) { System.out.println("IO: " + e.getMessage());
+	} finally { if(aSocket != null) aSocket.close();}
+}
+	*/
+}
---
+++ server.java
@@ -0,0 +1,47 @@
+
+import java.net.*;
+import java.io.*;
+
+//UDP Server
+public class server {
+	public static void main(String args[]) throws Exception {
+		DatagramSocket serverSocket = new DatagramSocket(1234);
+		BufferedReader serverInput = new BufferedReader(new InputStreamReader(System.in));
+		InetAddress ip = InetAddress.getByName("localhost");
+		byte outData[] = new byte[1024];
+		byte inData[] = new byte[1024];
+
+		while(true) {
+			DatagramPacket packet0 = new DatagramPacket(inData, inData.length);
+			serverSocket.receive(packet0);
+			String data = new String(packet0.getData());
+			System.out.println(data);
+
+			InetAddress ipAdd = packet0.getAddress();
+			int port = packet0.getPort();
+			String sendData = serverInput.readLine();
+			//to send data
+			//outData = sendData.getBytes();
+			outData = data.getBytes();
+			DatagramPacket packet1 = new DatagramPacket(outData, outData.length, ipAdd, port);
+			serverSocket.send(packet1);
+		}
+		/*
+		DatagramSocket aSocket = null;
+		try {
+			aSocket = new DatagramSocket(6789);
+			byte[] buffer = new byte[1000];
+			while(true) {
+				DatagramPacket request = new DatagramPacket(buffer,  buffer.length);
+				aSocket.receive(request);
+				DatagramPacket reply = new DatagramPacket(request.getData(),
+						request.getLength(), request.getAddress(), request.getPort());
+				aSocket.send(reply);
+			}
+		}catch (SocketException e) {System.out.println("Socket: " + e.getMessage());
+	}catch (IOException e) { System.out.println("IO: " + e.getMessage());
+	}finally { if(aSocket != null) aSocket.close(); }
+
+		*/
+}
+}
